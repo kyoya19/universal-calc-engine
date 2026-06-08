eab246a476dd375cc4d79fabc9eea1107293a986
kyoya19 <138812952+kyoya19@users.noreply.github.com>
2026-06-09 08:57:53 +0900

    Export generated target solver gated wrapper

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,5 @@
 export * from './model';
 export * from './tex';
 export * from './state_generation';
+export * from './generated_target_solver_policy';
+export * from './generated_target_solver_adapter';
