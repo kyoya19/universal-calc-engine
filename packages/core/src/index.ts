653d9cb0df7af7de8f4207097088929520787409
kyoya19 <138812952+kyoya19@users.noreply.github.com>
2026-06-09 11:42:44 +0900

    Export Android-oriented TeX display rows

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,6 @@
 export * from './model';
 export * from './tex';
+export * from './android_tex_display';
 export * from './state_generation';
 export * from './generated_target_solver_policy';
 export * from './generated_target_solver_adapter';
