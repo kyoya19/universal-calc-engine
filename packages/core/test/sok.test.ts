2734338e391ee979ac9304f7cfb8a2958191176e
kyoya19 <138812952+kyoya19@users.noreply.github.com>
2026-06-15 01:22:15 +0900

    sok

---
+++ packages/core/test/sok.test.ts
@@ -0,0 +1,7 @@
+import { expect, it } from 'vitest';
+import { isReportStatusSummaryOk } from '../src';
+
+it('sok', () => {
+  expect(isReportStatusSummaryOk({ ok: 1, warning: 0, rejected: 0, info: 1 })).toBe(true);
+  expect(isReportStatusSummaryOk({ ok: 1, warning: 1, rejected: 0, info: 1 })).toBe(false);
+});
