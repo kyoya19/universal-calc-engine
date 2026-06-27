433c326117b838389f133c017098ff9936c7a283
kyoya19 <138812952+kyoya19@users.noreply.github.com>
2026-06-27 15:10:24 +0900

    Test rejected report status

--- packages/core/test/sok.test.ts
+++ packages/core/test/sok.test.ts
@@ -5,3 +5,7 @@ it('sok', () => {
   expect(isReportStatusSummaryOk({ ok: 1, warning: 0, rejected: 0, info: 1 })).toBe(true);
   expect(isReportStatusSummaryOk({ ok: 1, warning: 1, rejected: 0, info: 1 })).toBe(false);
 });
+
+it('sok rejected', () => {
+  expect(isReportStatusSummaryOk({ ok: 1, warning: 0, rejected: 1, info: 0 })).toBe(false);
+});
