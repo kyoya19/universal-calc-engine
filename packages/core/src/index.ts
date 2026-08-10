a7a065f6a26c2437088669600fc44c205f8cad29
kyoya19 <138812952+kyoya19@users.noreply.github.com>
2026-08-10 18:22:51 +0900

    Export finite decision process API

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -8,6 +8,7 @@ export * from './external_input';
 export * from './observations';
 export * from './forward_evaluation';
 export * from './forward_result_handoff';
+export * from './finite_decision_process';
 export * from './scenario_comparison';
 export * from './parameter_sensitivity';
 export * from './discrete_estimation';
