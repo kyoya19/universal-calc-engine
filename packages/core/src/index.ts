4c1e7b28d6716fc49f01d02f96fefdea5991fd0d
kyoya19 <138812952+kyoya19@users.noreply.github.com>
2026-08-11 03:46:17 +0900

    Export acyclic direct forward adapter

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,6 +4,7 @@ export * from './reward_axes';
 export * from './validation';
 export * from './solver_diagnostics';
 export * from './acyclic_direct_solver';
+export * from './acyclic_direct_forward_evaluation';
 export * from './parameterized_scalars';
 export * from './external_input';
 export * from './observations';
