0f5b8604a56c83e88f66ea898f4304568bf27161
kyoya19 <138812952+kyoya19@users.noreply.github.com>
2026-08-13 10:03:41 +0900

    Export Candidate F hidden-observation inference API

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -15,6 +15,7 @@ export * from './state_distribution';
 export * from './first_passage';
 export * from './model_family_identifiability';
 export * from './hidden_state_observation';
+export * from './hidden_observation_candidate_inference';
 export * from './finite_decision_process';
 export * from './finite_decision_materialization';
 export * from './scenario_comparison';
