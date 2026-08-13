43d0db91be808690ec7658c13d2f3810150411a2
kyoya19 <138812952+kyoya19@users.noreply.github.com>
2026-08-13 19:16:00 +0900

    Export Candidate R pairwise smoothing API

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -18,6 +18,7 @@ export * from './first_passage_candidate_inference';
 export * from './model_family_identifiability';
 export * from './hidden_state_observation';
 export * from './hidden_state_smoothing';
+export * from './hidden_state_pairwise_smoothing';
 export * from './hidden_observation_candidate_inference';
 export * from './independent_evidence_bundle_inference';
 export * from './same_trajectory_hidden_first_passage_joint_inference';
