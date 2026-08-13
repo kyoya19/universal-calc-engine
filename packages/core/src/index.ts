5b0bc3bbc20f58353bb21e2f2744235ec051b71e
kyoya19 <138812952+kyoya19@users.noreply.github.com>
2026-08-13 15:47:36 +0900

    Export Candidate M robust-decision API

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -20,6 +20,7 @@ export * from './hidden_observation_candidate_inference';
 export * from './independent_evidence_bundle_inference';
 export * from './same_trajectory_hidden_first_passage_joint_inference';
 export * from './observation_design';
+export * from './ambiguity_preserving_robust_decision';
 export * from './finite_decision_process';
 export * from './finite_decision_materialization';
 export * from './scenario_comparison';
